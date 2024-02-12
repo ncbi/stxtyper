e46454ce85be7021721cb7338cf85e3d0a7ff995
Arjun Prasad <aprasad@ncbi.nlm.nih.gov>
2024-02-12 14:43:50 -0500

    Added synthetic tests (PD-4817)

--- test_stxtyper.sh
+++ test_stxtyper.sh
@@ -29,4 +29,21 @@ else
     fi
 fi
 
+if ! $STXTYPER -n test/synthetics.fa > test/synthetics.got
+then
+    echo "not ok: $STXTYPER returned a non-zero exit value indicating a failure of the software"
+    echo "#  $STXTYPER -n test/synthetics.fa > test/synthetics.got"
+    exit 1
+else
+    if ! diff -q test/synthetics.expected test/synthetics.got
+    then
+        echo "not ok: $STXTYPER returned output different from expected"
+        echo "#  $STXTYPER -n test/synthetics.fa > test/synthetics.got"
+        echo "# diff test/synthetics.expected test/synthetics.got"
+        diff test/synthetics.expected test/synthetics.got
+        echo "#  To approve run:"
+        echo "#     mv test/synthetics.got test/synthetics.expected "
+        exit 1
+    fi
+fi
 echo "ok, all tests passed"
